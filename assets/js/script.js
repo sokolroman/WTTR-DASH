d5302335493a68f98648fb5b4b3e9997d36e1690
Roman Sokol <sokolroman@proton.me>
2022-09-28 12:47:18 -0400

    second commit with javascript changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
+var APIkey = "ca3f6bd3972414b632534a7de05cbcbc";
+
+var searchInput = document.querySelector("#search-text");
+var searchForm = document.querySelector("#search-form");
+var searchesList = document.querySelector("#search-list");
+
+var city = document.getElementById("city")
+var temp = document.getElementById("temp")
+var highs = document.getElementById("highs")
+var lows = document.getElementById("lows")
+var humidity = document.getElementById("humidity")
+var windSpeed = document.getElementById("wind-speed")
+var conditions = document.getElementById("conditions")
 
 var date = new Date();
 	var current_date = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+ date.getDate();
@@ -5,3 +18,75 @@ var date = new Date();
 	var date_time = current_date+" "+current_time;	
 	document.getElementById("time-date").innerHTML = date_time;
 console.log(date_time)
+
+function getApi() {
+    var cityText = document.querySelector("#search-text").value;
+    console.log(cityText)
+
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + cityText + "&id=524901&units=imperial&appid=" + APIkey)
+
+    .then(function (response) {
+        return response.json();
+})}
+
+// //function to hold the fetch and data handling.
+var todos = [];
+
+// The following function renders items in a todo list as <li> elements
+function renderTodos() {
+  // Clear todoList element and update todoCountSpan
+  searchesList.innerHTML = "";
+
+  // Render a new li for each todo
+  for (var i = 0; i < todos.length; i++) {
+    var todo = todos[i];
+
+    var li = document.createElement("li");
+    li.textContent = todo;
+    li.setAttribute("data-index", i);
+
+    searchesList.appendChild(li);
+  }
+}
+
+// This function is being called below and will run when the page loads.
+function init() {
+  // Get stored todos from localStorage
+  var storedTodos = JSON.parse(localStorage.getItem("todos"));
+
+  // If todos were retrieved from localStorage, update the todos array to it
+  if (storedTodos !== null) {
+    todos = storedTodos;
+  }
+
+  // This is a helper function that will render todos to the DOM
+  renderTodos();
+}
+
+function storeTodos() {
+  // Stringify and set key in localStorage to todos array
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+// Add submit event to form
+searchForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  var todoText = searchInput.value.trim();
+
+  // Return from function early if submitted todoText is blank
+  if (todoText === "") {
+    return;
+  }
+
+  // Add new todoText to todos array, clear the input
+  todos.push(todoText);
+  searchInput.value = "";
+
+  // Store updated todos in localStorage, re-render the list
+  storeTodos();
+  renderTodos();
+});
+
+// Calls init to retrieve data and render it to the page on load
+init()
