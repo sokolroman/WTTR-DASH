161549ab8bc3e6c28586147204ba7f1fed068e67
Roman Sokol <sokolroman@proton.me>
2022-12-18 11:15:28 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,119 +1,63 @@
-var APIkey = "ca3f6bd3972414b632534a7de05cbcbc";
+const APIkey = "ca3f6bd3972414b632534a7de05cbcbc";
 
-var searchInput = document.querySelector("#search-text");
-var searchForm = document.querySelector("#search-form");
-var searchesList = document.querySelector("#search-list");
+// GIVEN a weather dashboard with form inputs
+const searchForm = document.querySelector("#search-form");
+const searchInput = document.querySelector("#search-text");
 
-var lat 
-var lon 
-
-var city = document.getElementById("city")
-
-var temp = document.getElementById("temp")
-var humidity = document.getElementById("humidity")
-var windSpeed = document.getElementById("wind-speed")
-
-var date = new Date();
-	var current_date = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+ date.getDate();
-	var current_time = date.getHours()+":"+date.getMinutes()+":"+ date.getSeconds();
-	var date_time = current_date+" "+current_time;	
-	document.getElementById("time-date").innerHTML = date_time;
-console.log(date_time)
-
-async function getApi() {
-    var cityText = document.querySelector("#search-text").value;
-    console.log(cityText)
-
-    response = await fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + cityText + "&id=524901&units=imperial&appid=" + APIkey)
-
-    .then(function (response) {
-
-// var data = response.json()
-response.json().then(function(data){
-  // console.log(data.city.coord)
-  console.log(data.city)
-
-city.innerHTML = data.city.name
-
-var lat = data.city.coord.lat
-var lon = data.city.coord.long
-
-fetch("api.openweathermap.org/data/2.5/forecast?lat=" + lat + lon + "&appid=" + APIkey)
-.then(function (response){
-
-  // var data = response.json()
-  response.json().then(function(data){
-    console.log(data)
-  })
-})
-
-})})}
-
-
-
-
-// //function to hold the fetch and data handling.
-var todos = [];
-
-// The following function renders items in a todo list as <li> elements
-function renderTodos() {
-  // Clear todoList element and update todoCountSpan
-  searchesList.innerHTML = "";
-
-  // Render a new li for each todo
-  for (var i = 0; i < todos.length; i++) {
-    var todo = todos[i];
-
-    var li = document.createElement("li");
-    li.textContent = todo;
-    li.setAttribute("data-index", i);
-
-    searchesList.appendChild(li);
-  }
-}
-
-// This function is being called below and will run when the page loads.
-function init() {
-  // Get stored todos from localStorage
-  var storedTodos = JSON.parse(localStorage.getItem("todos"));
-
-  // If todos were retrieved from localStorage, update the todos array to it
-  if (storedTodos !== null) {
-    todos = storedTodos;
-  }
-  // This is a helper function that will render todos to the DOM
-  renderTodos();
-}
-
-function storeTodos() {
-  // Stringify and set key in localStorage to todos array
-  localStorage.setItem("todos", JSON.stringify(todos));
-}
-
-// Add submit event to form
-searchForm.addEventListener("submit", function(event) {
+// WHEN I search for a city
+searchForm.addEventListener("submit", event => {
   event.preventDefault();
 
-  var todoText = searchInput.value.trim();
-
-getApi()
-
-  // Return from function early if submitted todoText is blank
-  if (todoText === "") {
-    return;
-
+  // THEN I am presented with current and future conditions for that city and that city is added to the search history
+  const city = searchInput.value.trim();
+  if (city) {
+    displayCurrentWeather(city);
+    displayFiveDayForecast(city);
+    addCityToSearchHistory(city);
   }
-
-  // Add new todoText to todos array, clear the input
-  todos.push(todoText);
-  searchInput.value = "";
-
-  // Store updated todos in localStorage, re-render the list
-  storeTodos();
-  renderTodos();
 });
 
+// WHEN I view current weather conditions for that city
+async function displayCurrentWeather(city) {
+  try {
+    // THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${APIkey}`);
+    const data = await response.json();
 
+    // Display city name
+    document.querySelector("#current-city").textContent = data.name;
 
-// Calls init to retrieve data and render it to the page on load
-init()
+    // Display date
+    const date = new Date();
+    document.querySelector("#current-date").textContent = date.toDateString();
+
+    // Display icon representing weather conditions
+    const iconUrl = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    document.querySelector("#current-icon").setAttribute("src", iconUrl);
+
+    // Display temperature
+    document.querySelector("#current-temp").textContent = `Temperature: ${data.main.temp}°F`;
+
+    // Display humidity
+    document.querySelector("#current-humidity").textContent = `Humidity: ${data.main.humidity}%`;
+
+    // Display wind speed
+    document.querySelector("#current-wind-speed").textContent = `Wind Speed: ${data.wind.speed} mph`;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// WHEN I view future weather conditions for that city
+async function displayFiveDayForecast(city) {
+  try {
+    // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${APIkey}`);
+    const data = await response.json();
+
+    // Clear any existing forecast items
+    document.querySelector("#forecast-list").innerHTML = "";
+
+    // Get forecast data for every 8th item (corresponds to every 3rd hour)
+    for (let i = 0; i < data.list.length;
+
